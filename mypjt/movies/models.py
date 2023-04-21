9d5a2c1b84d14d1c56abd1593722545cc83a8ee3
ryunayeon <yoonayeon@gmail.com>
2023-04-21 10:24:13 +0900

    model

--- mypjt/movies/models.py
+++ mypjt/movies/models.py
@@ -5,12 +5,14 @@ class Actor(models.Model):
     name = models.CharField(max_length=100)
 
 class Movie(models.Model):
+    actors = models.ManyToManyField(Actor)
     title = models.CharField(max_length=100)
     overview = models.TextField()
     release_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     poster_path = models.TextField()
 
+
 class Review(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
